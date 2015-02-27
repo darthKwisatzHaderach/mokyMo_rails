3131d1e2fe48077f29169efc95c68eb0c59fa28e
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-02-27 18:03:22 +0500

    #5: Create model, view and controller

---
+++ app/models/check_list.rb
@@ -0,0 +1,2 @@
+class CheckList < ActiveRecord::Base
+end
---
+++ test/controllers/check_lists_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CheckListsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
