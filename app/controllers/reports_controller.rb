c592a24dc9e7ede36173ead2c708c055521f48ef
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-03-18 15:23:40 +0500

    #13: Added prawn gem and simple pdf export

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,18 @@
+#encoding: utf-8
+
 class ReportsController < ApplicationController
-  def show
-    @check_list = CheckList.find(params[:id])
+  def show  	
+    @check_lists = CheckList.all
     respond_to do |format|
-      #format.html
+      format.html
       format.pdf do
         pdf = Prawn::Document.new
-        pdf.text "Hello world"
+          @check_lists.each do |list|
+          	pdf.font "/home/dmitriy/RubymineProjects/mokyMo/app/assets/fonts/pfdintextpro-regular.ttf"
+          	pdf.text "#{list.title}".encode('utf-8')          	
+          	pdf.text "#{list.description}".encode('utf-8')
+          	pdf.text " "
+          end
         send_data pdf.render, filename: "list.pdf", type: "application/pdf", disposition: "inline"
       end
     end
