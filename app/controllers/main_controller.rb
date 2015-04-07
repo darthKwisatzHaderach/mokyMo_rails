5d9493c76cfced62ca512410b01fb54369ef3f7d
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-04-07 15:41:38 +0500

    Bugfixes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,16 +2,18 @@ class MainController < ApplicationController
   def index
     test_objects = TestObject.where(component_id: @current_state.component)
     @test_object = test_objects.last
-    @execution = test_objects.last.executions.last
+    @execution = test_objects.last.executions.last    
     if @execution.nil?
       @pass = 'Нет данных'
       @fail = 'Нет данных'
       @not_implemented = 'Нет данных'
+      @comment = 'Нет данных'
     else
       r = @execution.results
       @pass = r.select { |item| item[:results] == true }.count
       @fail = r.select { |item| item[:results] == false }.count
       @not_implemented = r.select { |item| item[:implemented] == false }.count
+      @comment = @execution.comment
     end
   end
 end
