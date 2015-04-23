f956a655722a6f07c0b5e94be0c5069de4a60e10
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-04-23 18:08:09 +0500

    Added chart about whole project

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -43,15 +43,53 @@ class MainController < ApplicationController
         fail = r.select { |item| item[:status_kind_id] == 2 }.count
         not_implemented = r.select { |item| item[:status_kind_id] == 3 }.count
         pending = r.select { |item| item[:status_kind_id] == 4 }.count
-        result = ["#{t.version}\n #{e.created_at.strftime('%d/%m/%Y - %H:%M')}", pass, fail, not_implemented, pending]
+        result = [
+          "#{t.version}\n #{e.created_at.strftime('%d/%m/%Y - %H:%M')}",
+          pass,
+          fail,
+          not_implemented,
+          pending
+        ]
         @results << result
       end
     end
-    header = ['Выполнено', 'Провалено', 'Не выполнялось', 'В работе']
+    @header = ['Выполнено', 'Провалено', 'Не выполнялось', 'В работе']
     @data = (1..4).map do |i|
       {
-        name: header[i - 1],
+        name: @header[i - 1],
         data: @results.map { |x| [x[0], x[i]] }
+      }
+    end
+
+    @all = []
+    components = Project.find_by_id(current_state.project.id).components
+    components.each do |c|      
+      unless c.test_objects.count == 0
+        t = c.test_objects.last
+        unless t.executions.count == 0
+          e = t.executions.last
+          unless e.nil?
+            pass = e.results.select { |item| item[:status_kind_id] == 1 }.count
+            fail = e.results.select { |item| item[:status_kind_id] == 2 }.count
+            not_implemented = e.results.select { |item| item[:status_kind_id] == 3 }.count
+            pending = e.results.select { |item| item[:status_kind_id] == 4 }.count
+            result = [
+              "#{t.component.title}\n#{t.version}\n#{e.created_at.strftime('%d/%m/%Y - %H:%M')}",
+              pass,
+              fail,
+              not_implemented,
+              pending
+            ]
+            @all << result
+          end
+        end
+      end
+    end
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{@all}"
+    @g_data = (1..4).map do |i|
+      {
+        name: @header[i - 1],
+        data: @all.map { |x| [x[0], x[i]] }
       }
     end
   end
